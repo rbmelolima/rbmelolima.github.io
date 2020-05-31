4e7ed32a22a66637388ebd0031f8cd251d206c8c
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2020-05-31 20:48:02 -0300

    refactor: modificando o BrowserRouter para HashRouter

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { HashRouter, Route, Switch } from 'react-router-dom'
 
 import Home from './pages/Home';
 import Projects from './pages/Projects';
@@ -7,12 +7,12 @@ import About from './pages/About';
 
 export default function Routes() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route path="/" exact component={Home}></Route>
         <Route path="/projects" component={Projects}></Route>
         <Route path="/About" component={About}></Route>
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
